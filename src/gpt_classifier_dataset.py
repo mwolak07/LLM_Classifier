92a155973eca393d6288da6fa56467bbb7188fe5
Mateusz Wolak <mwolak07@gmail.com>
2023-04-06 14:29:45 -0400

    Added Classifier Dataset class stub

---
+++ src/gpt_classifier_dataset.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+
+class GPTClassifierDataset:
+    """
+    Responsible for providing an easy-to-use interface for creating and accessing the dataset for the GPT LLM
+    classification problem.
+    Responsible for reading MS_MARCO, applying the LLM to the prompts from MS_MARCO, and storing and loading this info
+    to/from an sqlite3 database.
+    """
+    pass
